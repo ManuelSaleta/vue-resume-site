9b7f49d89eaf307d95e4f6065be4e0880e71ef4a
ManuelSaleta <manny2206@gmail.com>
2026-03-06 17:21:23 -0500

    minor css

--- src/pages/PoemsPage.vue
+++ src/pages/PoemsPage.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <div class="container mx-auto px-4 py-8 flex flex-col items-center">
     <h1 class="text-3xl font-bold mb-6">Slice of Life Poems</h1>
     <div v-if="loading" class="text-center text-gray-500">Loading poems...</div>
     <div v-else class="space-y-8">
-      <div
-        v-for="poem in poems"
-        :key="poem.number"
-        class="poem border rounded-lg p-4 bg-white shadow-sm"
-      >
-        <h2 class="text-xl font-semibold mb-2">{{ poem.title }}</h2>
-        <pre class="whitespace-pre-wrap text-gray-700">{{ poem.body }}</pre>
+      <div v-for="poem in poems" :key="poem.number" class="mb-6 w-2xl">
+        <!-- <h2 class="text-xl font-semibold mb-2">{{ poem.title }}</h2>
+        <pre class="whitespace-pre-wrap text-gray-700">{{ poem.body }}</pre> -->
+        <v-card>
+          <v-card-title >{{ poem.number }}. {{ poem.title }}</v-card-title>
+          <v-card-text>{{ poem.body }}</v-card-text>
+        </v-card>
       </div>
     </div>
   </div>
@@ -23,6 +23,7 @@ import {
   orderBy,
   getDocs,
   QuerySnapshot,
+  limit,
 } from "firebase/firestore";
 import type { DocumentData } from "firebase/firestore";
 
@@ -33,7 +34,8 @@ const loading = ref<boolean>(true);
 onMounted(async () => {
   try {
     const poemCollection = collection(db, "poems");
-    const q = query(poemCollection, orderBy("number", "asc"));
+    //TODO: Get a random set of poems instead of the first 10, maybe using a random start point or a random selection method
+    const q = query(poemCollection, orderBy("number", "asc"), limit(10)); // Limit to 10 poems for free version
 
     const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(q);
 
